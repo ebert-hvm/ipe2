9b4687a570a602d00246bb5798784103fc6cd5f1
Varad_comrad <fabricio_asfora_002@hotmail.com>
2022-11-21 18:53:49 -0300

    todos os arquivos .py 2.0

--- resources.py
+++ resources.py
@@ -1,23 +1,32 @@
 import pyvisa as visa
 import time as tm
+import pandas as pd
 
 
-
-class Recourses:
+class Resources:
     def __init__(self) -> None:
         self.resources = visa.ResourceManager()
-        self.multimeter = self.resources.open_resource("") 
-        self.power_supply = self.resources.open_resource("")
+        self.multimeter = self.resources.open_resource("GPIB0::22::INSTR") 
+        self.power_supply = self.resources.open_resource("USB0::0x0957::0x8B18::MY51144612::0::INSTR")
+        values = []
     
-    def start(self, curr_lim, volt_lim) -> None:
+    def start(self, curr_lim: float, volt_lim: float) -> None:
         self.power_supply.write("OUTP ON")
         tm.sleep(5)
         self.power_supply.write(f"CURR:LIM {str(curr_lim)}")
-        self.power_supply.write(f"CURR:VOLT {str(volt_lim)}")
+        self.power_supply.write(f"VOLT:LIM {str(volt_lim)}")
         
-    def power_supply_setting(self,volt, curr):
-        pass
+    def power_supply_setting(self,volt: float, curr: float) -> None:
+        self.power_supply.write(f"VOLT {str(volt)}")
+        self.power_supply.write(f"CURR {str(curr)}")
+
+    def read_value(self) -> None:
+        self.values.append(self.multimeter.query('READ?'))
 
     def finish(self) -> None:
         tm.sleep(2)
         self.power_supply.write("OUTP OFF")
+    
+    def to_data_frame(self):
+        df = pd.DataFrame(self.values, columns= 'medidas')
+        return df
