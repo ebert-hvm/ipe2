e227c788e3cc9a6c5642a682dd8c775cf8df1822
ebert <ebert.hv.melo@gmail.com>
2022-12-01 13:30:48 -0300

    Table and Resources working

--- ipe/table.py
+++ ipe/table.py
@@ -2,13 +2,16 @@ import tkinter as tk
 import numpy as np
 
 class Table():
-    def __init__(self, root, row, col, tables_number):
+    def __init__(self, root, configure_data):
         self.root = root
-        self.row = row
-        self.col = col      
-        self.tables_number = tables_number
+        self.label = tk.StringVar(self.root) 
+        self.row = configure_data['rows']
+        self.col = configure_data['columns']
+        self.tables_number = configure_data['measures']
         self.cur = 0
-        self.tables = np.zeros((self.tables_number,self.row,self.col)).tolist()
+        #self.tables = np.array()
+        self.tables = [[['' for k in range(self.col)] for j in range(self.row)] for i in range(self.tables_number)]
+        #self.tables = [[['']*self.col]*self.row]*self.tables_number
         self.grid = self.make_grid()
   
         self.switch_table = self.modify_table()
@@ -17,18 +20,18 @@ class Table():
         grid = []
         for i in range(self.row):
             labelc = tk.Label(self.root, text=str(i+1))
-            labelc.grid(row=i+1, column=4)
+            labelc.grid(row=i+2, column=4)
             lst = []
             for j in range(self.col):
                 s = tk.StringVar(self.root)
                 s.set('')
                 e = tk.Entry(self.root, width=10, bd= 5, textvariable=s)
-                e.grid(row=i+1, column=j+5)
+                e.grid(row=i+2, column=j+5)
                 lst.append(s)
             grid.append(lst)
         for i in range(self.col):
             label = tk.Label(self.root, text=str(i+1))
-            label.grid(row=0, column=5+i)
+            label.grid(row=1, column=5+i)
   
         return grid
     
@@ -37,24 +40,39 @@ class Table():
         #table = self.tables[self.cur]
         # self.tables[self.cur][i][j].delete(0, tk.END)
         # self.tables[self.cur][i][j].insert(tk.END, val)
-        pass
 
     def modify_table(self):
         previous_button = tk.Button(self.root, text= 'Anterior', bg='#DDDDDD', width=10, command= lambda switch =-1 :self.switch_tables(switch))
         next_button = tk.Button(self.root, text= 'Próximo', bg='#DDDDDD', width=10, command= lambda switch = 1 :self.switch_tables(switch))
-        previous_button.grid(row=self.row+1, column=5)
+        self.label.set(f'Medição {self.cur+1}')
+        label = tk.Label(self.root, textvariable=self.label)
+        label.grid(row=0, column=5)
+        
+        previous_button.grid(row=self.row+2, column=5)
         if self.col == 1:
-            next_button.grid(row=self.row+1, column=5+self.col)
+            next_button.grid(row=self.row+2, column=5+self.col)
         else:   
-            next_button.grid(row=self.row+1, column=4+self.col)
+            next_button.grid(row=self.row+2, column=4+self.col)
 
     def switch_tables(self, switch):
         if self.tables_number > self.cur+switch and self.cur+switch >-1:
             self.cur += switch
-            print(self.cur)
+            self.label.set(f'Medição {self.cur+1}')
+            #print(self.cur)
         else:
-            return 0
+            return
         for i in range(self.row):
             for j in range(self.col):
-                self.switch(self.tables[self.cur][i][j],i,j)
+                #print(self.tables[self.cur][i][j])
+                #print(self.cur)
+                self.grid[i][j].set(self.tables[self.cur][i][j])
+        #print(self.tables)
+    def write_cell(self, val, cur, i, j):
+        if cur == self.cur:
+            self.grid[i][j].set(str(val))
+        self.tables[cur][i][j] = str(val)
+        #self.tables[cur][i][j] = 4
+
+        print(self.tables)
+        #print(self.tables.__type__)
         
